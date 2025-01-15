077cb8b1774a651820236beab14a3e005b326853
Sasha Gerrand <sgerrand@users.noreply.github.com>
2025-01-15 16:59:19 +0000

    Fix compilation errors
    ```
        error: undefined variable "description"
        │
      8 │      description: description,
        │                   ^^^^^^^^^^^
        │
        └─ mix.exs:8:19: PlugSessionRedis.Mixfile.project/0
    
        error: undefined variable "package"
        │
      9 │      package: package,
        │               ^^^^^^^
        │
        └─ mix.exs:9:15: PlugSessionRedis.Mixfile.project/0
    
        error: undefined variable "deps"
        │
     12 │      deps: deps]
        │            ^^^^
        │
        └─ mix.exs:12:12: PlugSessionRedis.Mixfile.project/0
    
        warning: function deps/0 is unused
        │
     29 │   defp deps do
        │        ~
        │
        └─ mix.exs:29:8: PlugSessionRedis.Mixfile (module)
    
        warning: function description/0 is unused
        │
     23 │   defp description do
        │        ~
        │
        └─ mix.exs:23:8: PlugSessionRedis.Mixfile (module)
    
        warning: function package/0 is unused
        │
     37 │   defp package do
        │        ~
        │
        └─ mix.exs:37:8: PlugSessionRedis.Mixfile (module)
    
    ** (CompileError) mix.exs: cannot compile module PlugSessionRedis.Mixfile (errors have been logged)
    ```

--- mix.exs
+++ mix.exs
@@ -2,14 +2,16 @@ defmodule PlugSessionRedis.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :plug_session_redis,
-     version: "0.1.0",
-     elixir: "~> 1.0",
-     description: description,
-     package: package,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :plug_session_redis,
+      version: "0.1.0",
+      elixir: "~> 1.0",
+      description: description(),
+      package: package(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
